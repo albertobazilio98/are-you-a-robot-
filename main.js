b9c6a3908818e270f99aa59d4602f1e53919b1ce
Beto <albertobazilio@jera.com.br>
2020-08-11 20:18:05 -0400

    [NEW] classifier

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 import { config } from 'dotenv-flow';
 import Discord from 'discord.js';
+import natural from 'natural';
 import doSetup from './src/setup';
 
 const client = new Discord.Client();
+let trainedClassifier;
+let classifier;
+classifier = natural.BayesClassifier.load('classifier.json', natural.PorterStemmerPt, (err, loadedClassifier) => {
+  if (err) return;
+  console.log(loadedClassifier);
+
+  classifier = loadedClassifier;
+  trainedClassifier = loadedClassifier;
+  trainedClassifier.train();
+});
 
 config();
 doSetup(client);
+
+client.on('messageReactionAdd', (messageReaction) => {
+  const message = messageReaction.message.content;
+  // eslint-disable-next-line no-underscore-dangle
+  const reaction = messageReaction._emoji.name;
+  const tokenizedMessage = natural.PorterStemmerPt.tokenizeAndStem(message);
+  classifier.addDocument(tokenizedMessage, reaction);
+});
+
+client.on('message', (message) => {
+  // message.react('🍆');
+  if (message.content === 'train' && message.author.id === '206124480928415744') {
+    classifier.save('classifier.json', (err, savedClassifier) => {
+      savedClassifier.train();
+      trainedClassifier = savedClassifier;
+    });
+  }
+  if (message.content.startsWith('test')) {
+    const text = message.content.slice(4);
+    console.log(trainedClassifier.getClassifications(text));
+    message.channel.send(trainedClassifier.classify(text));
+  }
+});
